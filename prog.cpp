8f36b87ff21b9b6a2420d99cbabc644891112fcb
zakhar <zaharchertuhin@gmail.com>
2021-05-16 14:57:55 +0400

    added homework

--- prog.cpp
+++ prog.cpp
@@ -2,3 +2,31 @@
 // Created by Admin on 16.05.2021.
 //
 
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <random>
+using namespace std;
+
+int Select_map(map<int, int>& m){
+    int sr = 0;
+    for(int i = 1; i < 32; i++ ){
+        int temperature = rand() % 18;
+        cout << temperature << endl;
+        m[i]= temperature;
+        sr+=temperature;
+    }
+    sr /= 31;
+//    cout << sr;
+    ofstream file ("C:\\Users\\Admin\\Desktop\\date.txt", ios::trunc);
+    for(int i = 1; i < 32; i++ ) {
+        if (m[i] > sr) {
+            file << "day " << i <<": "<< m[i]-sr<< endl;
+        }
+    }
+}
+
+int main() {
+    map<int, int> temp_day;
+    Select_map(temp_day);
+}
