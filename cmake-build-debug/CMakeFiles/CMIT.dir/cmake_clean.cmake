e154783e53e81534d20f697a8d396ea6a88f02de
zakhar <zaharchertuhin@gmail.com>
2021-05-30 13:39:47 +0400

    added homework

---
+++ cmake-build-debug/CMakeFiles/CMIT.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMIT.exe"
+  "CMIT.exe.manifest"
+  "CMIT.pdb"
+  "CMakeFiles/CMIT.dir/prog.cpp.obj"
+  "libCMIT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMIT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
